3b08d02eb260106b46c5870e22a416924189a719
Yaratch_Blake <Yaratch_Blake@student.mahoningctc.com>
2020-12-09 12:52:48 -0500

    first commit

---
+++ JS Output Challenge/script.js
@@ -0,0 +1,37 @@
+// Enter your code below to edit index.html
+
+yourName = "Blake"
+document.getElementById("yourName").innerHTML = yourName;
+
+aboutMe="My last name is Yaratch"
+document.getElementById("aboutMe").innerHTML = aboutMe;
+
+myBio="My middle name is Lee"
+document.getElementById("myBio").innerHTML = myBio;
+
+collegeChoice="I dont plan on going to college"
+document.getElementById("collegeChoice").innerHTML=collegeChoice
+
+programChoice="I dont know what a program is"
+document.getElementById("programChoice").innerHTML=programChoice
+
+collegeReason="I feel like a lot of the time at college, the money you put into is lost and you get a job in a different feild or you get a job that someone who didnt go to college can get."
+document.getElementById("collegeReason").innerHTML=collegeReason
+
+careerChoice="Honestly have no idea :/  Other than coding"
+document.getElementById("careerChoice").innerHTML=careerChoice
+
+companyChoice="Some where big like Microsoft or something of that sort."
+document.getElementById("companyChoice").innerHTML=companyChoice
+
+careerReason="I want money"
+document.getElementById("careerReason").innerHTML=careerReason
+
+answerOne="an application programming interface"
+document.getElementById("answerOne").innerHTML=answerOne
+
+answerTwo="innerHTML, document.write(), window.alert(), console.log()"
+document.getElementById("answerTwo").innerHTML=answerTwo
+
+answerThree="DOM"
+document.getElementById("answerThree").innerHTML=answerThree
